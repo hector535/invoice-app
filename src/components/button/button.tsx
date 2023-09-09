c6cce98e7ae8022a27fbfe6c6bf3b15838072227
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-09-09 10:34:28 -0400

    Remove link implementation from button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,44 +1,28 @@
-import { Link } from "react-router-dom";
 import clsx from "clsx";
-import { AnchorProps, ButtonProps, ElementProps } from "./button.type";
+import { ButtonProps } from "./button.type";
 import styles from "./button.module.scss";
 
-export const Button = (props: ElementProps) => {
+export const Button = (props: ButtonProps) => {
   const {
-    as = "button",
     variant = "primary",
     icon,
     className,
     children,
+    ...restProps
   } = props;
 
-  const classes = {
-    className: clsx(
-      styles.button,
-      styles[variant],
-      { [styles.icon]: !!icon },
-      className
-    ),
-  };
-
-  const content = (
-    <>
+  return (
+    <button
+      className={clsx(
+        styles.button,
+        styles[variant],
+        { [styles.icon]: !!icon },
+        className
+      )}
+      {...restProps}
+    >
       {!!icon && icon}
       {children}
-    </>
-  );
-
-  if (as === "a") {
-    return (
-      <Link {...(props as AnchorProps)} {...classes}>
-        {content}
-      </Link>
-    );
-  }
-
-  return (
-    <button {...(props as ButtonProps)} {...classes}>
-      {content}
     </button>
   );
 };
--- src/components/button/button.type.ts
+++ src/components/button/button.type.ts
@@ -1,15 +1,5 @@
-import { Link } from "react-router-dom";
-
 export type ButtonProps = {
-  as?: "button";
-} & React.ComponentPropsWithoutRef<"button">;
-
-export type AnchorProps = {
-  as?: "a";
-} & React.ComponentProps<typeof Link>;
-
-export type ElementProps = {
   icon?: React.ReactNode;
-  variant?: "default" | "primary" | "secondary" | "tertiary" | "danger";
+  variant?: "primary" | "secondary" | "tertiary" | "danger";
   children?: React.ReactNode;
-} & (ButtonProps | AnchorProps);
+} & React.ComponentPropsWithoutRef<"button">;
