c5138d4f58f413957fc5d0a5fb9f958dee96088d
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-09-15 16:17:06 -0400

    Import the new route configuration object.

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClientProvider } from "react-query";
-import { Navigate, RouterProvider, createHashRouter } from "react-router-dom";
-import { InvoiceLayout } from "@/features/invoices";
-import { Invoices } from "@/pages/invoices/invoices";
-import { NewInvoice } from "@/pages/new-invoice/new-invoice";
-import { EditInvoice } from "@/pages/edit-invoice/edit-invoice";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import { queryClient } from "./lib/react-query";
+import { appRoutes } from "./routes";
 import "../sass/style.scss";
 
-const router = createHashRouter([
-  {
-    element: <InvoiceLayout />,
-    children: [
-      {
-        path: "/invoices",
-        element: <Invoices />,
-      },
-      {
-        path: "/invoices/new",
-        element: <NewInvoice />,
-      },
-      {
-        path: "/invoices/:invoiceId/edit",
-        element: <EditInvoice />,
-      },
-    ],
-  },
-  {
-    path: "*",
-    element: <Navigate to="/invoices" />,
-  },
-]);
+const router = createHashRouter(appRoutes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
