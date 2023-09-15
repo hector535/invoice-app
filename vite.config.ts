3d515ca94c33ba389a98e6ac9352bbd3a88b01c5
Hector Figuereo <hectorfiguereo26@gmail.com>
2023-09-15 16:19:00 -0400

    Configure import aliases for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import autoprefixer from "autoprefixer";
@@ -7,9 +7,14 @@ import autoprefixer from "autoprefixer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
   resolve: {
     alias: {
-      "@": path.resolve("./src"),
+      "@": path.resolve(__dirname, "./src"),
+      "@tests": path.resolve(__dirname, "./tests"),
     },
   },
   css: {
